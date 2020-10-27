fc0a4593b9717b4262184377610407bf3177119d
ekkyregita28 <regitalaurent@gmail.com>
2020-10-27 17:54:48 +0700

    Halaman Login

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import '../../styles/index.css'
+
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-text">
+                <h1>Hello Friend!</h1>
+                <h6>Enter your personal details and start journey with us</h6>
+            </div>
+            <div className="sidebar-btn">
+                <button type="button" class="btn btn-primary">Sign Up</button>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
