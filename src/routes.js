26e6169b4c33287adb58b7c6a11621c73fcdf6cf
ekkyregita28 <regitalaurent@gmail.com>
2020-11-03 13:07:40 +0700

    thanking page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Dashboard from './containers/Dashboard'
 import Login from './containers/Login'
+import Thanks from './containers/Thanks'
 
 export const APP_ROUTE = [
     {
@@ -14,6 +15,13 @@ export const APP_ROUTE = [
         path: "/login",
         exact: true,
         component: Login,
+        restricted: true,
+    },
+    {
+        name: "Thanks",
+        path: "/thanks",
+        exact: true,
+        component: Thanks,
         restricted: true,
     },
 ];
