8c8d72fa01779523348541c7b8b9afde9edffe11
tudeverdhangga <tudeverdhangga@gmail.com>
2020-11-03 22:22:43 +0800

    Login with API

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react'
 import '../../styles/index.css'
 import { Navbar, Nav } from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
+import { isLogin, logout } from "../../utils/auth";
+import { useHistory } from "react-router-dom";
 
+const NavBar = () => {
+    const history = useHistory();
+    
+    const onLogout = () => {
+        logout();
+        history.replace("/");
+    };
 
-class NavBar  extends Component {
-    render() {
-        return (
-            <div className="navbar-nav">
-                <Navbar expand="lg">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#home" className="menu">Home</Nav.Link>
-                            <Nav.Link href="#link" className="menu">Features</Nav.Link>
-                            <Nav.Link href="#mitra" className="menu">Kemitraaan</Nav.Link>
-                            <Nav.Link href="#aboutus" className="menu">About Us</Nav.Link>
-                        </Nav>
-                        <Nav className="auth">
-                            <Nav.Link href="/login">Sign In</Nav.Link>
-                            <div className="line">|</div>
-                            <Nav.Link href="#signup">Sign Up</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-        )
-    }
+    return (
+        <div className="navbar-nav">
+            <Navbar expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home" className="menu">Home</Nav.Link>
+                        <Nav.Link href="#link" className="menu">Features</Nav.Link>
+                        <Nav.Link href="#mitra" className="menu">Kemitraaan</Nav.Link>
+                        <Nav.Link href="#aboutus" className="menu">About Us</Nav.Link>
+                    </Nav>
+                    <Nav className="auth">
+                        {isLogin ? (
+                            <Nav.Link onClick={onLogout}>Logout</Nav.Link>
+                        ) : (
+                            <React.Fragment>
+                                <Nav.Link href="/login">Sign In</Nav.Link>
+                                <div className="line">|</div>
+                                <Nav.Link href="#signup">Sign Up</Nav.Link>
+                            </React.Fragment>
+                        )}   
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
 }
 
 export default NavBar
