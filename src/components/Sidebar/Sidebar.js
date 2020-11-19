3be5aaafcdc97e284c24415200a5dad051e74b11
tudeverdhangga <tudeverdhangga@gmail.com>
2020-11-19 15:28:58 +0800

    Fix conflit sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -11,15 +11,9 @@ const Sidebar = () => {
                 <h6>Enter your personal details and start journey with us</h6>
             </div>
             <div className="sidebar-btn">
-<<<<<<< HEAD
                 <Nav.Link href="/register">
                     <button type="button" class="btn btn-primary">
                         <h4>Sign Up</h4>
-=======
-                <Nav.Link href="/Register">
-                    <button type="button" className="btn btn-primary">
-                        Sign Up
->>>>>>> 244baf4be5e3313a8855470b3f753841eb196c1d
                     </button>
                 </Nav.Link>
             </div>
