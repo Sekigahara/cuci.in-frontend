8974a8680bbc76f780c3dd24462d5c9f7c443d29
tudeverdhangga <tudeverdhangga@gmail.com>
2021-01-11 01:45:06 +0800

    Apply Partnership Form

--- src/components/Form/ApplyForm.js
+++ src/components/Form/ApplyForm.js
@@ -1,216 +1,186 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../../styles/index.css'
 import laundry from '../../styles/laundry-pict.jpg'
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Container, Col, Form, Nav } from 'react-bootstrap'
 import { MdSupervisorAccount, MdMarkunreadMailbox, MdLocalLaundryService } from "react-icons/md"
-import { APPLYFORM, USERID, STOREOWNER, GETOWNER } from '../../utils/url'
-import { user } from '../../utils/auth'
+import { APPLYFORM, STATUS_APPLY } from '../../utils/url'
+import { user, getToken } from '../../utils/auth'
 import { FaPhoneAlt } from "react-icons/fa"
-import Thanks from '../../containers/Thanks'
+import { useHistory } from "react-router-dom";
 import axios from 'axios';
 
-class ApplyForm extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            owner_id: 0,
-            name: "",
-            phone: "",
-            address: "",
-            rating: 0,
-            manhours: 0,
-            laundry_type:[]
-        };
+const ApplyForm = () => {
+    const config = {
+        headers: { Authorization: `Bearer ${getToken()}` }
+    };
 
-        this.handleInputChange = this.handleInputChange.bind(this);
+    useEffect(() => {
+        axios
+            .get(STATUS_APPLY, config)
+            .then((res) => {
+				if (res.data.data === "Not Approved"){
+                    history.push("/thanks");
+                }
+                console.log(res.data.data);
+            }).catch((err) => {
+                alert("Something Wrong" + " (" + err.message + ")");
+                console.log(err);
+            }); 
+    }, []);
+
+    const [ownername, setOwnername] = React.useState("");
+    const [phone, setPhone] = React.useState("");
+    const [address, setAddress] = React.useState("");
+    const allowedState = [
+        { name: "laundry_type_1", value: "Regular" },
+        { name: "laundry_type_2", value: "Express" },
+        { name: "laundry_type_3", value: "Bed Cover" },
+        { name: "laundry_type_4", value: "Footwear" }
+      ];      
+    const [laundry_type, setLaundryType] = React.useState([]);
+    const handleChange = event => {
+        setLaundryType(laundry_type.concat(event.target.value));
+    };
+    const history = useHistory();
+
+    const onSubmit = () => {
+        axios
+            .post(APPLYFORM, {
+                user_id: user(),
+                ownername: ownername,
+                phone: phone,
+                address: address,
+                laundry_type: JSON.stringify(laundry_type)
+            }, config)
+            .then((res) => {
+				console.log(res);
+                history.push("/thanks");
+            }).catch((err) => {
+                alert("Something Wrong" + " (" + err.message + ")");
+                console.log(err);
+            });        
     }
 
-    onOwnerIdChange = e => {
-        this.setState({
-            owner_id: e.target.value
-        });
-    };
+    return (
+        <div className="form-box">
+        <Container>
+            <Form>
+                <div className="left-side">
+                    <div className="left-fragment">
+                        <Form.Group className="form-group1" controlId="formBasicOwner">
+                            <Col sm={10}>
+                                <div className="input-icon">
+                                <div className='aplly-icon'><MdSupervisorAccount /></div>
+                                    <Form.Control 
+                                        className = "no-border" 
+                                        type="text" placeholder="Owner Name"
+                                        value={ownername}
+                                        onChange={(e) => setOwnername(e.target.value)}
+                                    />
+                                </div>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group className="form-group1" controlId="formBasicPhone">
+                            <Col sm={10}>
+                                <div className="input-icon">
+                                <div className='aplly-icon'><FaPhoneAlt /></div>
+                                    <Form.Control 
+                                        className = "no-border" 
+                                        type="text" placeholder="Phone"
+                                        value={phone}
+                                        onChange={(e) => setPhone(e.target.value)}
+                                    />
+                                </div>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group className="form-group1" controlId="formBasicAddress">
+                            <Col sm={10}>
+                                <div className="input-icon">
+                                <div className='aplly-icon'><MdMarkunreadMailbox /></div>
+                                    <Form.Control 
+                                        className = "no-border" 
+                                        type="text" placeholder="Address"
+                                        value={address}
+                                        onChange={(e) => setAddress(e.target.value)}
+                                    />
+                                </div>
+                            </Col>
+                        </Form.Group>
 
-    onNameChange = e => {
-        this.setState({
-            name: e.target.value
-        });
-    };
-
-    onPhoneChange = e => {
-        this.setState({
-            phone: e.target.value
-        });
-    };
-
-    onAddressChange = e => {
-        this.setState({
-            address: e.target.value
-        });
-    };
-
-    onSubmit = () => {
-        //var usr_id = 0;
-        //getting user id by username
-        /*
-        axios
-            .get(USERID, { 
-                params: {
-                    username: user()
-                }
-            })
-            .then((e) => {
-                usr_id = e.id;
-            })
-            .then(function (response) {
-                console.log(response);
-            })
-        */
-
-        //store owner id by user id
-        axios
-            .post(STOREOWNER, {
-                params: {
-                    user_id: user()
-                }
-            }).then(res => console.log(res))
-        
-        //get owner by user id
-        axios
-            .get(GETOWNER, {
-                params: {
-                    user_id: user()
-                }
-            }).then((e) => {
-                this.onOwnerIdChange(e.id);
-            })
-            .then(function (response) {
-                console.log(response);
-            })
-
-        //packed all data inside array
-        const data = {
-            owner_id: this.state.id,
-            name: this.state.ownername,
-            phone: this.state.phone,
-            address: this.state.address,
-            rating: this.state.rating,
-            manhours: this.state.manhours,
-            laundry_type: this.state.laundry_type
-        };
-        axios
-            .post(APPLYFORM, data)
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
-    }
-
-    handleInputChange(event) {
-        const target = event.target;
-        var value = target.value;
-        
-        if(target.checked){
-            this.state.laundry_type[value] = value;   
-        }else{
-            this.state.laundry_type.splice(value, 1);
-        }
-    }
-
-    render() {
-        return (
-            <div className="form-box">
-            <Container>
-                <Form>
-                    <div className="left-side">
-                        <div className="left-fragment">
-                            <Form.Group className="form-group1" controlId="formBasicOwner">
-                                <Col sm={10}>
-                                    <div className="input-icon">
-                                    <div className='aplly-icon'><MdSupervisorAccount /></div>
-                                        <Form.Control 
-                                            className = "no-border" 
-                                            type="text" placeholder="Owner Name"
+                        <Form.Group className="form-group1" controlId="LaundryType">
+                            <Col sm={10}>
+                                <p className="form-type"><MdLocalLaundryService />&nbsp; Laundry Type 
+                                    <div className="form-check">
+                                        {allowedState.map((state, index) => (
+                                            <Form.Check
+                                                type="checkbox"
+                                                label={state.value}
+                                                name={state.name}
+                                                id="laundryType"
+                                                value={state.value}
+                                                onChange={handleChange}
+                                            />
+                                        ))}
+                                        {/* <Form.Check
+                                            type="checkbox"
+                                            label="Regular"
+                                            name="laundryType"
+                                            id="laundryType"
+                                            value="regular"
+                                            onChange={(e) => setLaundryType(e.target.value)}
                                         />
-                                    </div>
-                                </Col>
-                            </Form.Group>
-                            <Form.Group className="form-group1" controlId="formBasicPhone">
-                                <Col sm={10}>
-                                    <div className="input-icon">
-                                    <div className='aplly-icon'><FaPhoneAlt /></div>
-                                        <Form.Control 
-                                            className = "no-border" 
-                                            type="text" placeholder="Phone"
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="Express"
+                                            name="laundryType"
+                                            id="LaundryType2"
+                                            value="express"
+                                            onChange={(e) => setLaundryType(e.target.value)}
                                         />
-                                    </div>
-                                </Col>
-                            </Form.Group>
-                            <Form.Group className="form-group1" controlId="formBasicAddress">
-                                <Col sm={10}>
-                                    <div className="input-icon">
-                                    <div className='aplly-icon'><MdMarkunreadMailbox /></div>
-                                        <Form.Control 
-                                            className = "no-border" 
-                                            type="text" placeholder="Address"
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="Bed Cover"
+                                            name="laundryType"
+                                            id="LaundryType3"
+                                            value="bed cover"
+                                            onChange={(e) => setLaundryType(e.target.value)}
                                         />
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="Baby Stuff"
+                                            name="laundryType"
+                                            id="LaundryType4"
+                                            value="baby stuff"
+                                            onChange={(e) => setLaundryType(e.target.value)}
+                                        />
+                                        <Form.Check
+                                            type="checkbox"
+                                            label="Footwear"
+                                            name="laundryType"
+                                            id="LaundryType5"
+                                            value="footwear"
+                                            onChange={(e) => setLaundryType(e.target.value)}
+                                        /> */}
                                     </div>
-                                </Col>
-                            </Form.Group>
-
-                            <Form.Group className="form-group1" controlId="LaundryType">
-                                <Col sm={10}>
-                                    <p className="form-type"><MdLocalLaundryService />&nbsp; Laundry Type 
-                                        <div className="form-check">
-                                            <Form.Check
-                                                type="checkbox"
-                                                label="Regular"
-                                                name="LaundryType"
-                                                id="LaundryType1"
-                                            />
-                                            <Form.Check
-                                                type="checkbox"
-                                                label="Express"
-                                                name="LaundryType"
-                                                id="LaundryType2"
-                                            />
-                                            <Form.Check
-                                                type="checkbox"
-                                                label="Bed Cover"
-                                                name="LaundryType"
-                                                id="LaundryType3"
-                                            />
-                                            <Form.Check
-                                                type="checkbox"
-                                                label="Baby Stuff"
-                                                name="LaundryType"
-                                                id="LaundryType4"
-                                            />
-                                            <Form.Check
-                                                type="checkbox"
-                                                label="Footwear"
-                                                name="LaundryType"
-                                                id="LaundryType5"
-                                            />
-                                        </div>
-                                    </p>
-                                </Col>
-                            </Form.Group>
-                            </div>
-                            </div>
-                        <div className="right-side">
-                            <div className="right-fragment">
-                                <Form.Group>
-                                    <div className="right-fragment-center">
-                                        <img src={laundry} className="img-laundry"></img>
-                                        <Button /*href="/thanks" component={Thanks}*/ onChange={this.onSubmit} type="submit" className="rounded-pill btn-submit" style={{ backgroundColor: "#0063E3" }}><h5 className="button-text">Join Us !</h5></Button>
-                                    </div>
-                                </Form.Group>
-                            </div>
+                                </p>
+                            </Col>
+                        </Form.Group>
                         </div>
-                    </Form>
-                </Container>
-            </div>
-        )
-    }
+                        </div>
+                    <div className="right-side">
+                        <div className="right-fragment">
+                            <Nav.Link onClick={onSubmit}>
+                                <div className="right-fragment-center">
+                                    <img src={laundry} className="img-laundry"></img>
+                                    <button type="button" className="rounded-pill btn-submit" style={{ backgroundColor: "#0063E3" }}><h5 className="button-text">Join Us !</h5></button>
+                                </div>
+                            </Nav.Link>
+                        </div>
+                    </div>
+                </Form>
+            </Container>
+        </div>
+    )
 }
 
 export default ApplyForm
--- src/utils/url.js
+++ src/utils/url.js
@@ -1,8 +1,8 @@
+import { user } from "./auth";
+
 const BASE_URL = 'http://localhost:8000/api/auth';
 
 export const LOGIN = `${BASE_URL}/login`;
-export const APPLYFORM = `${BASE_URL}/outlet`;
-export const USERID = `${BASE_URL}/user`;
-export const STOREOWNER = `${BASE_URL}/owner`;
-export const GETOWNER = `${BASE_URL}/owner/user_id`;
+export const APPLYFORM = `${BASE_URL}/partnership/apply`;
+export const STATUS_APPLY = `${BASE_URL}/partnership/status/${user()}`;
 export const REGISTER = `${BASE_URL}/register`;
