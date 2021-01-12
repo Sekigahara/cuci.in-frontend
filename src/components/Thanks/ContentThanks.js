704a8cb8cced923d30c966689ea62620eb4e4a13
tudeverdhangga <tudeverdhangga@gmail.com>
2021-01-12 21:09:33 +0800

    Add Download Desktop File

--- src/components/Thanks/ContentThanks.js
+++ src/components/Thanks/ContentThanks.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
 import '../../styles/index.css'
 import pict from '../../styles/laundry-pict.jpg';
-
+import { Nav } from 'react-bootstrap'
+import { DOWNLOAD } from '../../utils/url'
+import { getToken } from '../../utils/auth'
+import axios from 'axios';
 
 const ContentThanks = () => {
+    const config = {
+        headers: { Authorization: `Bearer ${getToken()}` }
+    };
+
+    const onSubmit = () => {
+        axios
+            .get(DOWNLOAD, config)
+            .then((res) => {
+                console.log(res);
+            });        
+    }
+
     return (
         <div className="thanks-background">
             <div className="thanks-box">  
@@ -14,6 +29,12 @@ const ContentThanks = () => {
                     <h1>Terimakasih Telah Bergabung !</h1>
                     <h6>Selamat datang di Cuci-In!</h6>
                     <h6>Semoga pelayanan kami membantu bisnis laundry Anda</h6>
+                </div>
+                <br></br>
+                <div className="download-content text-center">
+                    <p className="download-text">Keep exploring with us.
+                        <Nav.Link onClick={onSubmit}>Download Desktop</Nav.Link>
+                    </p>
                 </div>
             </div>
         </div>
--- src/utils/url.js
+++ src/utils/url.js
@@ -6,3 +6,4 @@ export const LOGIN = `${BASE_URL}/login`;
 export const APPLYFORM = `${BASE_URL}/partnership/apply`;
 export const STATUS_APPLY = `${BASE_URL}/partnership/status/${user()}`;
 export const REGISTER = `${BASE_URL}/register`;
+export const DOWNLOAD = `${BASE_URL}/download`;
